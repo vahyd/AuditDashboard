ea5e87b3becca0f4152ab717fa44f9a692a97160
Vahid Khatami <vahyd.khatami@gmail.com>
2018-09-08 12:09:03 -0400

    Add files via upload

--- global.r
+++ global.r
@@ -1,5 +1,9 @@
+if(!require(ploylt)){   install.packages("plotly")   library(plotly) }
+if(!require(shiny)){   install.packages("shiny")   library(shiny) }
+
 # Read CSV into R
 
 MyPath <- paste(getwd(),"sample_data.csv",sep="/")
 MyData <- as.data.frame(read.csv(MyPath, header=TRUE, sep=","))
 
+df <- read.csv(paste(getwd(),"map.csv",sep="/"), encoding = 'UTF-8')
