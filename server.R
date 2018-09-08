92a8789ed5f9790e9f8adf520f3585122b761c4d
Vahid Khatami <vahyd.khatami@gmail.com>
2018-09-08 11:50:06 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -2,9 +2,17 @@ function(input, output, session) {
   
   # Combine the selected variables into a new data frame
   selectedData <- reactive({
-    MyData = MyData[MyData$Report.year == "2017", ]
+    if(input$year != "All" && input$sector != "All"){
+      temp = MyData[MyData$Report.year == input$year,]
+      temp[temp$sector== input$sector, ]
+    }else if(input$year == "All" && input$sector == "All")
+      MyData
+    else if (input$sector == "All")
+      MyData[MyData$Report.year == input$year, ]
+    else if (input$year == "All")
+      MyData[MyData$sector == input$sector, ]
   })
   
-  output$table <- renderDataTable(MyData)
+  output$table <- renderDataTable(selectedData())
   
 }
--- ui.R
+++ ui.R
@@ -1,8 +1,9 @@
 pageWithSidebar(
   headerPanel('Audit Dashboard'),
   sidebarPanel(
-    selectInput('year', 'Select Year', MyData['Report.year']),
-    selectInput('sector', 'Select Sector', MyData['sector'])
+    selectInput('year', 'Select Year',choices = c("All",(unique(MyData$Report.year))),selected = "All"),
+    
+    selectInput('sector', 'Select Sector', choices = c("All",levels(unique(MyData$sector))),selected = "All")
                 
   ),
   mainPanel(
